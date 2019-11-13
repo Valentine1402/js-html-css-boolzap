fa2ba225582df6b5965b02db70b484b80c9b5ca0
valentina <valentinagiannone@gmail.com>
2019-11-13 23:05:54 +0100

    completato milestone 2

--- boolzap/js/script.js
+++ boolzap/js/script.js
@@ -1,56 +1,62 @@
 //funzione generale jquery
 $( document ).ready(function(){
 
- //agganciamo al click sul bottone la funzione di callbnack
+ // Funzione che mi permette di inviare il messaggio dell'utente nel caso si prema il tasto Invio, cambiando anche l'icona vicino all'input
+ // invio mess
+
+ $('.message').keydown(function(event){
+   if (event.which==13) {
+     inviaMessaggioUtente();
+   }
+   else {
+     $('.writing i').addClass("fa-paper-plane");
+   }
+ });
+
+ // Funzione con cui vado a ricambiare l'icona una volta che la barra perde il focus
+ $(".message").focusout(function(){
+   $(".writing i").removeClass("fa-paper-plane");
+ });
+
+
+ // azioni che mi permettono di inviare un messaggio sullo schermo
   $(".fa-microphone").click(function(){
-
-  //ci salviamo il valore dell'input inserito dall'utente
   var messaggio = $(".message").val();
-  console.log(messaggio);
-
-
-  // cloniamo il div che sta dentro template
   var elmentmsg = $("#template .msgsent").clone();
-
-  // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
   elmentmsg.find(".msg").text(messaggio);
-
-  // appendiamo una copia con testo valorizzato del div "msgsent"
   var stampa = $("#verde").append(elmentmsg);
-
-  // ripuliamo il contenuto dell'input, per UX
   $(".message").val("");
-
-//aggiunto piccola animazione sms
+  //aggiunto piccola animazione sms
   $(".msgsent").animate({
       left: '450px',
       opacity: '0.8',
-      height: '50px',
+      height: '40px',
       width: '150px'
     });
-
+    // funzione per generare risposta automatica
+    setTimeout(function () {
+     var answer = $("#template .sendbyCPU").clone();
+     answer.find(".msg").text("Ciao, come stai?");
+     $("#bianco").append(answer);
+    }, 1000);
   })
 
-  // Funzione che mi permette di cercare un contatto nella barra dei contatti
-  $('.search-bar').keyup(function(event){
-    var cerca_Nome=$(this).val().toLowerCase();
 
-      $('.friends').each(function(){
-        var nomeCercato=$(this).find('b').text().toLowerCase();
-        if (nomeCercato.includes(cerca_Nome)) {
-          $(this).show();
-        }
-        else {
-          $(this).hide();
-        }
-      });
+ // Funzione che mi permette di cercare un contatto nella barra dei contatti
+ $('.search-bar').keyup(function(event){
+   var cercaNome=$(this).val().toLowerCase();
+
+     $('.friends').each(function(){
+       var nomeCercato=$(this).find('b').text().toLowerCase();
+       if (nomeCercato.includes(cercaNome)) {
+         $(this).show();
+       }
+       else {
+         $(this).hide();
+       }
+   });
 
   });
 
 
-
-
-
-
-
- });
+});
